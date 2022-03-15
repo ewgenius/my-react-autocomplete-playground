097ea59584b133dcacbb9a18f30dba7e4bd24608
Evgenii Khramkov <ewgeniux@gmail.com>
2022-03-15 22:45:48 +0300

    WIP: use real api for testing

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,17 @@ const items: Item[] = new Array(100).fill(null).map((_, id) => ({
 }));
 
 function fetcher(q: string): Promise<Item[]> {
-  return new Promise((r) => r(items.filter((item) => item.value.includes(q))));
+  return fetch("https://api.publicapis.org/entries")
+    .then((r) => r.json())
+    .then(({ entries }) =>
+      (entries as any[])
+        .map((e, id) => ({
+          id,
+          value: e.API,
+        }))
+        .filter((item) => item.value.includes(q))
+    );
+  // return new Promise((r) => r(items.filter((item) => item.value.includes(q))));
 }
 
 export function App() {
